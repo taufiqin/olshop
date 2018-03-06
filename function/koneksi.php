c05248424bd2e9d0e1379dabccc8d1e214c1e339
Unknown <raisulmuhalani@student.telkomuniversity.ac.id>
2018-03-06 15:26:00 +0700

    delete

--- function/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$server = "localhost";
-	$username = "root";
-	$password = "1";
-	$database = "olshop";
-	
-	$koneksi = mysqli_connect($server, $username, $password, $database) or die("koneksi ke database gagal");
-	 
