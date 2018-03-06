ef4545ed4d655acbf5c7c136bdef5c59cf4bb771
Unknown <seviamegakusuma@student.telkomuniversity.ac.id>
2018-03-06 15:32:54 +0700

    delete all

--- function/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "olshop";
-	
-	$koneksi = mysqli_connect($server, $username, $password, $database) or die("koneksi ke database gagal");
-	 
